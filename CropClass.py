0fcb19dbf2e548d75ea2abf08b70c1d111360c08
mattgregory525 <30012@longroad.ac.uk>
2014-06-17 19:52:08 +0100

    Task 5 completed

--- CropClass.py
+++ CropClass.py
@@ -1,3 +1,4 @@
+import random
 class Crop:
     #Doc string
     """ A simulation of a generic food crop"""
@@ -37,21 +38,50 @@ class Crop:
         elif self._growth == 0 :
             self._status = "Seed"
         
-
-
     def grow(self,light,water):
         if light >= self._light_need and water >= self._water_need:
             self._growth += self._growth_rate
         self._days_growing += 1
         self._update_status()
         
+def auto_grow(crop,days):
+    for day in range(days):
+        light = random.randint(1,10)
+        water = random.randint(1,10)
+        crop.grow(light,water)
 
+def manual_grow(crop):
+    valid = False
+    while not valid:
+        try:
+            light = int(input("Please enter a light value (1-10): "))
+            if 1<= light <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between(1-10)")
+                        
+        except ValueError:
+            print("Value entered not valid - please enter a value between(1-10)")
+
+    valid = False
+    while not valid:
+        try:
+            water = int(input("Please enter a water value (1-10): "))
+            if 1<= water <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between(1-10)")
+                        
+        except ValueError:
+            print("Value entered not valid - please enter a value between(1-10)")
+
+    crop.grow(light,water)
 def main():
     #Instaniate class
     new_crop = Crop(1,4,3)
     print (new_crop.needs())
     print (new_crop.report())
-    new_crop.grow(4,4)
+    manual_grow(new_crop)
     print(new_crop.report())
     
 
