1c28a3e97f9c9a3e1eb5cbc640f634cacacbd7bc
mattgregory525 <30012@longroad.ac.uk>
2014-06-17 19:36:11 +0100

    Task 4 completed

--- CropClass.py.py
+++ CropClass.py.py
@@ -24,36 +24,18 @@ class Crop:
         return {"Type": self._type, "Status": self._status, "Growth": self._growth, "Days growing": self._days_growing}
         
 
-    #Underscores make a class private, meaning they must be accsessed through another method
-    def _update_status(self):
-        if self._growth > 15:
-            self.status = "Old"
-        elif self._growth >10:
-            self._status = "Mature"
-        elif self._growth > 5:
-            self._status = "Young"
-        elif self._growth > 0:
-            self._status = "Seedling"
-        elif self._growth == 0 :
-            self._status = "Seed"
-        
 
 
-    def grow(self,light,water):
-        if light >= self._light_need and water >= self._water_need:
-            self._growth += self._growth_rate
-        self._days_growing += 1
-        self._update_status()
-        
+
+
+
+
 
 def main():
     #Instaniate class
     new_crop = Crop(1,4,3)
     print (new_crop.needs())
     print (new_crop.report())
-    new_crop.grow(4,4)
-    print(new_crop.report())
-    
 
 
 if __name__ == "__main__":
