0e7a0a9edb73e1eb318d11c557bf1dc0f9e164da
mattgregory525 <30012@longroad.ac.uk>
2014-06-23 19:01:49 +0100

    Task 9 completed

---
+++ FieldClass.py
@@ -0,0 +1,26 @@
+from PotatoClass import *
+from WheatClass import *
+from CowClass import *
+from SheepClass import *
+
+class Field:
+    """A field"""
+
+    def __init__(self,max_animals,max_crops):
+        self._crops = []
+        self._animals = []
+        self.max_animals = max_animals
+        self.max_crops = max_crops
+
+
+    
+
+def main():
+    new_field = Field(5,2)
+   
+
+    
+
+
+if __name__ == "__main__":
+    main()
