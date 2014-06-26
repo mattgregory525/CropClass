ec75d984a195ebeec2ef25066a4711441f44395d
mattgregory525 <30012@longroad.ac.uk>
2014-06-26 14:02:52 +0100

    Task 2B completed + task 2C started

--- Crop simulator.py
+++ Crop simulator.py
@@ -2,12 +2,42 @@ import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+from Radio_button_widget_class import *
+
 class CropWindow(QMainWindow):
     """This class creates the main window"""
 
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Crop simulator")
+        self.create_select_crop_layout()
+
+
+    def create_select_crop_layout(self):
+
+        self.crop_radio_buttons = RadioButtonWidget("Crop simulation", "Please select a choice", ("Wheat", "Potato"))
+        self.instantiate_button = QPushButton("Create crop")
+
+
+        self.initial_layout = QVBoxLayout()
+        self.initial_layout.addWidget(self.crop_radio_buttons)
+        self.initial_layout.addWidget(self.instantiate_button)
+
+        self.selected_crop_widget = QWidget()
+        self.selected_crop_widget.setLayout(self.initial_layout)
+
+        self.setCentralWidget(self.selected_crop_widget)
+
+        self.instantiate_button.clicked.connect(self.instantiate_crop)
+
+
+    def instantiate_crop(self):
+        crop_type = self.crop_radio_buttons.selected_button()
+        if crop_type ==1:
+            self.simulated_crop = Wheat()
+        elif crop_type ==2:
+            self.simulated_crop = Potato()
+        print(self.simulated_crop)
 
 def main():
     crop_simulation = QApplication(sys.argv)
--- Radio_button_widget_class.py
+++ Radio_button_widget_class.py
@@ -2,11 +2,11 @@ from PyQt4.QtGui import *
 
 class RadioButtonWidget(QWidget):
     """Create a group of radio buttons"""
-    def __init__(self,label,instructions,button_list):
+    def __init__(self,label,instruction,button_list):
         super().__init__()
 
         self.title_label = QLabel(label)
-        self.radio_group_box = (instruction)
+        self.radio_group_box = QGroupBox(instruction)
         self.radio_button_group = QButtonGroup()
 
         self.radio_button_list = []
@@ -15,10 +15,10 @@ class RadioButtonWidget(QWidget):
 
         self.radio_button_list[0].setChecked(True)
 
-        self.radio_button_layout = QBoxLayout()
+        self.radio_button_layout = QVBoxLayout()
 
         counter = 1
-        for each in radio_button_list:
+        for each in self.radio_button_list:
             self.radio_button_layout.addWidget(each)
             self.radio_button_group.addButton(each)
             self.radio_button_group.setId(each,counter)
